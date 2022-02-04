1814ba032e75c54b96391b37b6d2239b14eb27ae
Marc Johnson <marc.johnson27591@gmail.com>
2022-02-03 19:04:27 -0500

    [#1] add mage support

---
+++ magefile.go
@@ -0,0 +1,48 @@
+//go:build mage
+// +build mage
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	// "github.com/magefile/mage/mg" // mg contains helpful utility functions, like Deps
+)
+
+const (
+	executable string = "mp3.exe"
+)
+
+// Default target to run when none is specified
+// If not set, running mage will list available targets
+// var Default = Build
+
+// Create the executable image
+func Build() error {
+	// mg.Deps(InstallDeps)
+	fmt.Printf("Creating %s\n", executable)
+	cmd := exec.Command("go", "build", "-o", executable, "./cmd/mp3/")
+	return cmd.Run()
+}
+
+// A custom install step if you need your bin someplace other than go/bin
+// func Install() error {
+// 	mg.Deps(Build)
+// 	fmt.Println("Installing...")
+// 	return os.Rename("./MyApp", "/usr/bin/MyApp")
+// }
+
+// Manage your deps, or running package managers.
+// func InstallDeps() (e error) {
+// 	fmt.Println("Installing Deps...")
+// 	cmd := exec.Command("go", "get", "github.com/stretchr/piglatin")
+// 	return cmd.Run()
+// }
+
+// Delete the executable image
+func Clean() {
+	fmt.Printf("Removing %s\n", executable)
+	os.RemoveAll(executable)
+}
