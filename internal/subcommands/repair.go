de024bf2f61a604c956b74bc46cbc67b3dd6a40d
Marc Johnson <marc.johnson27591@gmail.com>
2022-02-05 18:04:09 -0500

    [#6] repair implementation
    - add -albums and -artists command line parameters (both default
      to ".*")
    - add validation of -target command line parameter, rejecting
      unrecognized values in favor of 'metadata'

--- internal/subcommands/repair.go
+++ internal/subcommands/repair.go
@@ -3,15 +3,24 @@ package subcommands
 import (
 	"flag"
 	"fmt"
+	"log"
 	"mp3/internal/files"
+	"strings"
 )
 
 type repair struct {
 	fs            *flag.FlagSet
 	target        *string
+	albumRegex    *string
+	artistRegex   *string
 	topDirectory  *string
 	fileExtension *string
 }
+
+const (
+	defaultRepairType string = "metadata"
+	fsRepair string = "files"
+)
 
 func (r *repair) Name() string {
 	return r.fs.Name()
@@ -21,7 +30,9 @@ func NewRepairCommand() *repair {
 	fSet := flag.NewFlagSet("repair", flag.ExitOnError)
 	return &repair{
 		fs:            fSet,
-		target:        fSet.String("target", "metadata", "either 'metadata' (make metadata agree with file system) or 'files' (make file system agree with metadata)"),
+		target:        fSet.String("target", defaultRepairType, fmt.Sprintf("either '%s' (make metadata agree with file system) or '%s' (make file system agree with metadata)", defaultRepairType, fsRepair)),
+		albumRegex:    fSet.String("albums", ".*", "regular expression of albums to repair"),
+		artistRegex:   fSet.String("artists", ".*", "regular epxression of artists to repair"),
 		topDirectory:  fSet.String("topDir", defaultTopDir, "top directory in which to look for music files"),
 		fileExtension: fSet.String("ext", files.DefaultFileExtension, "extension for music files"),
 	}
@@ -37,5 +48,22 @@ func (r *repair) Exec(args []string) {
 }
 
 func (r *repair) runSubcommand() {
-	fmt.Printf("%s: target: %s, top directory: %s, extension: %s\n", r.Name(), *r.target, *r.topDirectory, *r.fileExtension)
+	r.validateTarget()
+	var output []string
+	output = append(output, fmt.Sprintf(" target: %s", *r.target))
+	output = append(output, fmt.Sprintf(" albums: %s", *r.albumRegex))
+	output = append(output, fmt.Sprintf(" artists: %s", *r.artistRegex))
+	log.Printf("%s:%s", r.Name(), strings.Join(output, ";"))
+	log.Printf("search %s for files with extension %s", *r.topDirectory, *r.fileExtension)
+}
+
+func (r *repair) validateTarget() {
+	switch *r.target{
+	case defaultRepairType:
+	case fsRepair:
+	default:
+		log.Printf("-target=%s is not valid\n", *r.target)
+		s := defaultRepairType
+		r.target = &s
+	}
 }
