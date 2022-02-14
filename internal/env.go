b10b05eb02cefacee96b7c8928818b7aa6e9c8f1
Marc Johnson <marc.johnson27591@gmail.com>
2022-02-14 16:55:01 -0500

    [#19] add test coverage for internal/env.go

--- internal/env.go
+++ internal/env.go
@@ -6,22 +6,23 @@ import (
 )
 
 var (
-	HomePath  string
-	TmpFolder string
+	HomePath     string
+	TmpFolder    string
+	noTempFolder error = fmt.Errorf("no temporary folder defined, checked TMP and TEMP")
+	noHomePath   error = fmt.Errorf("no home path defined, checked HOMEPATH")
 )
 
-func LookupEnvVars() []error {
-	var errors []error
+func LookupEnvVars() (errors []error) {
 	var found bool
 	// get temporary folder
 	if TmpFolder, found = os.LookupEnv("TMP"); !found {
 		if TmpFolder, found = os.LookupEnv("TEMP"); !found {
-			errors = append(errors, fmt.Errorf("no temporary folder defined, checked TMP and TEMP"))
+			errors = append(errors, noTempFolder)
 		}
 	}
 	// get homepath
 	if HomePath, found = os.LookupEnv("HOMEPATH"); !found {
-		errors = append(errors, fmt.Errorf("no home path defined, checked HOMEPATH"))
+		errors = append(errors, noHomePath)
 	}
-	return errors
+	return
 }
---
+++ internal/env_test.go
@@ -0,0 +1,123 @@
+package internal
+
+import (
+	"errors"
+	"os"
+	"testing"
+)
+
+func TestLookupEnvVars(t *testing.T) {
+	type envState struct {
+		varName  string
+		varValue string
+		varSet   bool
+	}
+	var savedStates []envState
+	value, set := os.LookupEnv("TMP")
+	savedStates = append(savedStates, envState{varName: "TMP", varValue: value, varSet: set})
+	value, set = os.LookupEnv("TEMP")
+	savedStates = append(savedStates, envState{varName: "TEMP", varValue: value, varSet: set})
+	value, set = os.LookupEnv("HOMEPATH")
+	savedStates = append(savedStates, envState{varName: "HOMEPATH", varValue: value, varSet: set})
+	var savedTmpFolder = TmpFolder
+	var savedHomePath = HomePath
+	defer func() {
+		for _, ss := range savedStates {
+			if ss.varSet {
+				os.Setenv(ss.varName, ss.varValue)
+			} else {
+				os.Unsetenv(ss.varName)
+			}
+		}
+		TmpFolder = savedTmpFolder
+		HomePath = savedHomePath
+	}()
+	tests := []struct {
+		name          string
+		envs          []envState
+		wantTmpFolder string
+		wantHomePath  string
+		wantErrors    []error
+	}{
+		{
+			name: "expected use case",
+			envs: []envState{
+				{varName: "TMP", varValue: "/tmp", varSet: true},
+				{varName: "TEMP", varValue: "/tmp2", varSet: true},
+				{varName: "HOMEPATH", varValue: "/users/myUser", varSet: true},
+			},
+			wantTmpFolder: "/tmp", wantHomePath: "/users/myUser", wantErrors: nil,
+		},
+		{
+			name: "missing TMP",
+			envs: []envState{
+				{varName: "TMP", varValue: "", varSet: false},
+				{varName: "TEMP", varValue: "/tmp2", varSet: true},
+				{varName: "HOMEPATH", varValue: "/users/myUser", varSet: true},
+			},
+			wantTmpFolder: "/tmp2", wantHomePath: "/users/myUser", wantErrors: nil,
+		},
+		{
+			name: "missing TMP and TEMP",
+			envs: []envState{
+				{varName: "TMP", varValue: "", varSet: false},
+				{varName: "TEMP", varValue: "", varSet: false},
+				{varName: "HOMEPATH", varValue: "/users/myUser", varSet: true},
+			},
+			wantTmpFolder: "", wantHomePath: "/users/myUser", wantErrors: []error{noTempFolder},
+		},
+		{
+			name: "missing homepath",
+			envs: []envState{
+				{varName: "TMP", varValue: "/tmp", varSet: true},
+				{varName: "TEMP", varValue: "/tmp2", varSet: true},
+				{varName: "HOMEPATH", varValue: "", varSet: false},
+			},
+			wantTmpFolder: "/tmp", wantHomePath: "", wantErrors: []error{noHomePath},
+		},
+		{
+			name: "missing both vars",
+			envs: []envState{
+				{varName: "TMP", varValue: "", varSet: false},
+				{varName: "TEMP", varValue: "", varSet: false},
+				{varName: "HOMEPATH", varValue: "", varSet: false},
+			},
+			wantTmpFolder: "", wantHomePath: "", wantErrors: []error{noTempFolder, noHomePath},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// clear initial state
+			TmpFolder = ""
+			HomePath = ""
+			for _, env := range tt.envs {
+				if env.varSet {
+					os.Setenv(env.varName, env.varValue)
+				} else {
+					os.Unsetenv(env.varName)
+				}
+			}
+			if gotErrors := LookupEnvVars(); !equalErrorSlices(gotErrors, tt.wantErrors) {
+				t.Errorf("LookupEnvVars() = %v, want %v", gotErrors, tt.wantErrors)
+			}
+			if TmpFolder != tt.wantTmpFolder {
+				t.Errorf("TmpFolder = %v, want %v", TmpFolder, tt.wantTmpFolder)
+			}
+			if HomePath != tt.wantHomePath {
+				t.Errorf("HomePath = %v, want %v", HomePath, tt.wantHomePath)
+			}
+		})
+	}
+}
+
+func equalErrorSlices(got []error, want []error) bool {
+	if len(got) != len(want) {
+		return false
+	}
+	for k := 0; k < len(got); k++ {
+		if !errors.Is(got[k], want[k]) {
+			return false
+		}
+	}
+	return true
+}
