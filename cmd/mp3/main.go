301061181402ee1e2d45b217837cf1e48080e864
Marc Johnson <marc.johnson27591@gmail.com>
2022-02-22 19:41:49 -0500

    [#19] add unit tests for main package

--- cmd/mp3/main.go
+++ cmd/mp3/main.go
@@ -8,12 +8,13 @@ import (
 	"path/filepath"
 
 	"github.com/sirupsen/logrus"
+	"github.com/utahta/go-cronowriter"
 )
 
 func main() {
 	returnValue := 1
-	if initEnv() {
-		if initLogging() {
+	if initEnv(internal.LookupEnvVars) {
+		if initLogging(internal.TmpFolder) {
 			if cmd, args, err := subcommands.ProcessCommand(os.Args); err != nil {
 				fmt.Fprintln(os.Stderr, err)
 				logrus.Error(err)
@@ -26,8 +27,8 @@ func main() {
 	os.Exit(returnValue)
 }
 
-func initEnv() bool {
-	if errors := internal.LookupEnvVars(); len(errors) > 0 {
+func initEnv(lookup func() []error) bool {
+	if errors := lookup(); len(errors) > 0 {
 		fmt.Fprintln(os.Stderr, "1 or more environment variables unset")
 		for _, e := range errors {
 			fmt.Fprintln(os.Stderr, e)
@@ -37,13 +38,17 @@ func initEnv() bool {
 	return true
 }
 
-func initLogging() bool {
-	path := filepath.Join(internal.TmpFolder, "mp3", "logs")
+// exposed so that unit tests can close the writer!
+var logger *cronowriter.CronoWriter
+
+func initLogging(parentDir string) bool {
+	path := filepath.Join(parentDir, "mp3", "logs")
 	if err := os.MkdirAll(path, 0755); err != nil {
 		fmt.Fprintf(os.Stderr, "cannot create path '%s': %v\n", path, err)
 		return false
 	}
-	logrus.SetOutput(internal.ConfigureLogging(path))
+	logger = internal.ConfigureLogging(path)
+	logrus.SetOutput(logger)
 	internal.CleanupLogFiles(path)
 	return true
 }
---
+++ cmd/mp3/main_test.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"errors"
+	"os"
+	"testing"
+)
+
+func Test_initLogging(t *testing.T) {
+	type args struct {
+		parentDir string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{name: "current directory", args: args{parentDir: "."}, want: true},
+		{name: "non-existent directory", args: args{parentDir: "main_test.go"}, want: false},
+	}
+	defer func() {
+		logger.Close()
+		if err := os.RemoveAll("mp3"); err != nil {
+			t.Errorf("initLogging(): %v", err)
+		}
+	}()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := initLogging(tt.args.parentDir); got != tt.want {
+				t.Errorf("initLogging() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_initEnv(t *testing.T) {
+	type args struct {
+		lookup func() []error
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{name: "no errors", args: args{lookup: func() []error { return nil }}, want: true},
+		{name: "process errors", args: args{lookup: func() []error {
+			var e []error
+			e = append(e, errors.New("error 1"))
+			e = append(e, errors.New("error 2"))
+			return e
+		}}, want: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := initEnv(tt.args.lookup); got != tt.want {
+				t.Errorf("initEnv() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
