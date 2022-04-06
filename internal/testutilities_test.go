fb291e4c94086668f3765ebd394eede36b292d01
Marc Johnson <marc.johnson27591@gmail.com>
2022-04-06 16:46:46 -0400

    [#19] more testutilities testing

--- internal/testutilities_test.go
+++ internal/testutilities_test.go
@@ -1,6 +1,8 @@
 package internal
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -93,6 +95,55 @@ func TestDestroyDirectoryForTesting(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			DestroyDirectoryForTesting(tt.args.fnName, tt.args.dirName)
+		})
+	}
+}
+
+func TestPopulateTopDirForTesting(t *testing.T) {
+	fnName := "PopulateTopDirForTesting()"
+	topDirName := "testDir"
+	if err := Mkdir(topDirName); err != nil {
+		t.Errorf("%s error creating directory %q: %v", fnName, topDirName, err)
+	}
+	defer func() {
+		if err := os.RemoveAll(topDirName); err != nil {
+			t.Errorf("%s error destroying test directory %q: %v", fnName, topDirName, err)
+		}
+	}()
+	// force a quick error
+	artistDirName := CreateArtistNameForTesting(0)
+	if err := createFileForTesting(topDirName, artistDirName); err != nil {
+		t.Errorf("%s error creating file %q: %v", fnName, artistDirName, err)
+	}
+	type args struct {
+		topDir string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{{name: "force early failure", args: args{topDir: topDirName}, wantErr: true}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := PopulateTopDirForTesting(tt.args.topDir); (err != nil) != tt.wantErr {
+				t.Errorf("%s error = %v, wantErr %v", fnName, err, tt.wantErr)
+			}
+		})
+	}
+	// now eliminate the error
+	if err := os.Remove(filepath.Join(topDirName, artistDirName)); err != nil {
+		t.Errorf("%s error deleting file %q: %v", fnName, artistDirName, err)
+	}
+	tests = []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{{name: "success", args: args{topDir: topDirName}, wantErr: false}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := PopulateTopDirForTesting(tt.args.topDir); (err != nil) != tt.wantErr {
+				t.Errorf("%s error = %v, wantErr %v", fnName, err, tt.wantErr)
+			}
 		})
 	}
 }
