48cdd6c7b82f6fc815d2fed7bb3d674de6305883
Marc Johnson <marc.johnson27591@gmail.com>
2022-04-28 20:00:01 -0400

    [#34] remove -target option from repair subcommand

--- internal/subcommands/repair.go
+++ internal/subcommands/repair.go
@@ -2,7 +2,6 @@ package subcommands
 
 import (
 	"flag"
-	"fmt"
 	"io"
 	"mp3/internal"
 	"mp3/internal/files"
@@ -13,15 +12,9 @@ import (
 
 type repair struct {
 	n      string
-	target *string
 	dryRun *bool
 	sf     *files.SearchFlags
 }
-
-const (
-	defaultRepairType string = "metadata"
-	fsRepair          string = "files"
-)
 
 func (r *repair) name() string {
 	return r.n
@@ -34,7 +27,6 @@ func newRepair(fSet *flag.FlagSet) CommandProcessor {
 func newRepairSubCommand(fSet *flag.FlagSet) *repair {
 	return &repair{
 		n:      fSet.Name(),
-		target: fSet.String("target", defaultRepairType, fmt.Sprintf("either '%s' (make metadata agree with file system) or '%s' (make file system agree with metadata)", defaultRepairType, fsRepair)),
 		dryRun: fSet.Bool("dryRun", false, "if true, output what would have repaired, but make no repairs"),
 		sf:     files.NewSearchFlags(fSet),
 	}
@@ -48,15 +40,13 @@ func (r *repair) Exec(w io.Writer, args []string) {
 
 const (
 	logDryRunFlag string = "dryRun"
-	logTargetFlag string = "target"
 )
 
 func (r *repair) logFields() logrus.Fields {
-	return logrus.Fields{internal.LOG_COMMAND_NAME: r.name(), logDryRunFlag: *r.dryRun, logTargetFlag: *r.target}
+	return logrus.Fields{internal.LOG_COMMAND_NAME: r.name(), logDryRunFlag: *r.dryRun}
 }
 
 func (r *repair) runSubcommand() {
-	r.validateTarget()
 	logrus.WithFields(r.logFields()).Info(internal.LOG_EXECUTING_COMMAND)
 	switch *r.dryRun {
 	case true:
@@ -64,20 +54,5 @@ func (r *repair) runSubcommand() {
 	case false:
 		// TODO: replace with call to get files and perform the specified repairs
 		// logrus.Infof("search %s for files with extension %s for artists '%s' and albums '%s'", *r.ff.topDirectory, *r.ff.fileExtension, *r.ff.artistRegex, *r.ff.albumRegex)
-	}
-}
-
-func (r *repair) validateTarget() {
-	switch *r.target {
-	case defaultRepairType, fsRepair:
-	default:
-		fmt.Fprintf(os.Stderr, internal.USER_UNRECOGNIZED_VALUE, "-target", *r.target)
-		logrus.WithFields(logrus.Fields{
-			internal.LOG_COMMAND_NAME: r.name(),
-			internal.LOG_FLAG:         "-target",
-			internal.LOG_VALUE:        *r.target,
-		}).Warn(internal.LOG_INVALID_FLAG_SETTING)
-		s := defaultRepairType
-		r.target = &s
 	}
 }
