e29424a37e11beefb1b6c161f32799005301f20b
Marc Johnson <marc.johnson27591@gmail.com>
2023-01-16 17:19:27 -0500

    [#134] clean up flags

--- internal/flags_test.go
+++ internal/flags_test.go
@@ -3,35 +3,20 @@ package internal
 import "testing"
 
 func TestDecorateBoolFlagUsage(t *testing.T) {
-	fnName := "DecorateBoolFlagUsage()"
+	const fnName = "DecorateBoolFlagUsage()"
 	type args struct {
 		usage        string
 		defaultValue bool
 	}
-	tests := []struct {
-		name string
+	tests := map[string]struct {
 		args
 		want string
 	}{
-		{
-			name: "non-default value",
-			args: args{
-				usage:        "this is my boolean flag",
-				defaultValue: true,
-			},
-			want: "this is my boolean flag",
-		},
-		{
-			name: "non-default value",
-			args: args{
-				usage:        "this is my boolean flag",
-				defaultValue: false,
-			},
-			want: "this is my boolean flag (default false)",
-		},
+		"true default value":  {args: args{usage: "this is my boolean flag", defaultValue: true}, want: "this is my boolean flag"},
+		"false default value": {args: args{usage: "this is my boolean flag", defaultValue: false}, want: "this is my boolean flag (default false)"},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
 			if got := DecorateBoolFlagUsage(tt.args.usage, tt.args.defaultValue); got != tt.want {
 				t.Errorf("%s = %q, want %q", fnName, got, tt.want)
 			}
@@ -40,35 +25,20 @@ func TestDecorateBoolFlagUsage(t *testing.T) {
 }
 
 func TestDecorateIntFlagUsage(t *testing.T) {
-	fnName := "DecorateIntFlagUsage()"
+	const fnName = "DecorateIntFlagUsage()"
 	type args struct {
 		usage        string
 		defaultValue int
 	}
-	tests := []struct {
-		name string
+	tests := map[string]struct {
 		args
 		want string
 	}{
-		{
-			name: "non-default value",
-			args: args{
-				usage:        "this is my int flag",
-				defaultValue: 1,
-			},
-			want: "this is my int flag",
-		},
-		{
-			name: "non-default value",
-			args: args{
-				usage:        "this is my int flag",
-				defaultValue: 0,
-			},
-			want: "this is my int flag (default 0)",
-		},
+		"non-zero value": {args: args{usage: "this is my int flag", defaultValue: 1}, want: "this is my int flag"},
+		"zero value":     {args: args{usage: "this is my int flag", defaultValue: 0}, want: "this is my int flag (default 0)"},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
 			if got := DecorateIntFlagUsage(tt.args.usage, tt.args.defaultValue); got != tt.want {
 				t.Errorf("%s = %q, want %q", fnName, got, tt.want)
 			}
@@ -77,35 +47,20 @@ func TestDecorateIntFlagUsage(t *testing.T) {
 }
 
 func TestDecorateStringFlagUsage(t *testing.T) {
-	fnName := "DecorateStringFlagUsage()"
+	const fnName = "DecorateStringFlagUsage()"
 	type args struct {
 		usage        string
 		defaultValue string
 	}
-	tests := []struct {
-		name string
+	tests := map[string]struct {
 		args
 		want string
 	}{
-		{
-			name: "non-default value",
-			args: args{
-				usage:        "this is my string flag",
-				defaultValue: "foo",
-			},
-			want: "this is my string flag",
-		},
-		{
-			name: "non-default value",
-			args: args{
-				usage:        "this is my string flag",
-				defaultValue: "",
-			},
-			want: "this is my string flag (default \"\")",
-		},
+		"non-empty value": {args: args{usage: "this is my string flag", defaultValue: "foo"}, want: "this is my string flag"},
+		"empty value":     {args: args{usage: "this is my string flag", defaultValue: ""}, want: "this is my string flag (default \"\")"},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
 			if got := DecorateStringFlagUsage(tt.args.usage, tt.args.defaultValue); got != tt.want {
 				t.Errorf("%s = %q, want %q", fnName, got, tt.want)
 			}
